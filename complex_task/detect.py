e9c39f2dc84eb88ae22ba55fc0ea6151d6b1c28e
jalen <jalenchuh@gmail.com>
2023-09-15 16:30:20 +0800

    update

--- complex_task/detect.py
+++ complex_task/detect.py
@@ -6,18 +6,15 @@ class Position:
         self.center_point = [0, 0]
         self.vertices = [[0, 0], [0, 0], [0, 0], [0, 0]]
 
-def detect(frame):
+def detect():
     # TODO: xxxxx
     time_start = time.time()
-    # # 加载模型
-    # model = YOLO("yolov8n.pt")  # 加载预训练模型（建议用于训练）
+    # 加载模型
+    model = YOLO("/home/jalen/code/RoboMaster/visual/Assessment/complex_task/best.pt")  # 加载预训练模型（建议用于训练）
 
-    # # 使用模型
-    # model.train(data="/home/jalen/code/RoboMaster/visual/Assessment/complex_task/cola/data.yaml", epochs=3)  # 训练模型
-    # metrics = model.val()  # 在验证集上评估模型性能
-    # results = model("/home/jalen/code/RoboMaster/visual/Assessment/complex_task/cola/train/images/01_jpg.rf.54210228de63d663b8a729922c71adf2.jpg")  # 对图像进行预测
-    # success = model.export(format="onnx")  # 将模型导出为 ONNX 格式
-    # position = results.xyxy[0][0]
+    result = model("/home/jalen/code/RoboMaster/visual/Assessment/complex_task/cola/train/images/01_jpg.rf.54210228de63d663b8a729922c71adf2.jpg")  # 对图像进行预测
+
+    print(result.numpy())
 
     position = Position()
     # position.center_point
@@ -25,3 +22,6 @@ def detect(frame):
 
     time_end = time.time()
     return position, time_end - time_start
+
+if __name__ == '__main__':
+    detect()
