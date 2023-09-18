b41fa2a73b6a35e6401b68a6af697f3b817421eb
jalen <jalenchuh@gmail.com>
2023-09-18 19:46:39 +0800

    update ros2 task

--- ros2/src/task/src/find_red_node.cpp
+++ ros2/src/task/src/find_red_node.cpp
@@ -15,8 +15,8 @@ public:
     ImageSubscriber()
     : Node("image_subscriber") , red_count(0) {
         subscription_ = this->create_subscription<my_interfaces::msg::MyMat>(
-            "topic", 10, std::bind(&ImageSubscriber::topic_callback, this, _1));
-        
+            "raw_image", 10, std::bind(&ImageSubscriber::topic_callback, this, _1));
+
         publisher_ = this->create_publisher<std_msgs::msg::UInt8>("red_count", 10);
         timer_ = this->create_wall_timer(
             10ms, std::bind(&ImageSubscriber::timer_callback, this));
@@ -25,30 +25,39 @@ public:
 private:
     void topic_callback(const my_interfaces::msg::MyMat &msg) {
         auto cv_ptr = cv_bridge::toCvCopy(msg.image, sensor_msgs::image_encodings::TYPE_8UC3);
-        cv::Mat cv_image = cv_ptr->image;
+        auto cv_image = cv_ptr->image;
 
-        double fps = 0.0;   // TODO: calculate fps
-        cv::Mat hsv_image;  // HSV 图像
-        cv::Mat red_mask;   // 红色物体的 mask
-        cv::Scalar lower = cv::Scalar(0, 100, 100);  // 红色的 HSV 下界
-        cv::Scalar upper = cv::Scalar(10, 255, 255); // 红色的 HSV 上界
-        cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
+        double fps = 0.0;
+        cv::Mat hsv_image, red_mask;
+        RCLCPP_INFO(this->get_logger(), "get image '%s'", msg.time_stamp.c_str());
+
+        cvtColor(cv_image, hsv_image, cv::COLOR_BGR2HSV);
+
+        // 提取红色区域
+        auto lower = cv::Scalar(0, 100, 100);  // 红色的 HSV 下界
+        auto upper = cv::Scalar(10, 255, 255); // 红色的 HSV 上界
+        inRange(hsv_image, lower, upper, red_mask);
+
+        // 闭运算连接红色区域，开运算去噪点
+        auto kernel_close = getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(15, 15));
+        auto kernel_open = getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
+        morphologyEx(red_mask, red_mask, cv::MORPH_CLOSE, kernel_close);
+        morphologyEx(red_mask, red_mask, cv::MORPH_OPEN, kernel_open);
+
         std::vector<std::vector<cv::Point>> contours;
-
-        cv::cvtColor(cv_image, hsv_image, cv::COLOR_BGR2HSV);            // 转换颜色空间
-        cv::inRange(hsv_image, lower, upper, red_mask);                  // 获取红色物体的 mask
-        cv::morphologyEx(red_mask, red_mask, cv::MORPH_OPEN, kernel);    // 开运算去噪点
-        cv::morphologyEx(red_mask, red_mask, cv::MORPH_CLOSE, kernel);   // 闭运算连接红色区域
-        // cv::findContours(red_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-        // cv::drawContours(cv_image, contours, -1, cv::Scalar(0, 0, 255), 2);
+        findContours(red_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+        for(auto contour : contours) {
+            auto rect = boundingRect(contour);
+            rectangle(cv_image, rect, cv::Scalar(0, 0, 255), 2);
+        }
 
         red_count = contours.size();
+        RCLCPP_INFO(this->get_logger(), "find red'%d'", red_count);
         cv::putText(cv_image, "FPS: " + std::to_string(fps) + "\n" + msg.time_stamp,
                     cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
         cv::imshow("Result", cv_image);
-        // cv::waitKey(0);
-
-        RCLCPP_INFO(this->get_logger(), "get image '%s'", msg.time_stamp.c_str());
+        cv::imshow("RedMask", red_mask);
+        cv::waitKey(10);
     }
     void timer_callback() {
         auto message = std_msgs::msg::UInt8();
